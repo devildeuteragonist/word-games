1311fefbed7ad3ba5014e6f92d1df42a7b6827df
Finity_Cardinal <142924829+devildeuteragonist@users.noreply.github.com>
2024-12-30 22:15:25 +0000

    added potential easy mode

---
+++ the-guessing-game/wordgame-code-easy-mode.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3 
+import time
+
+time.sleep(1)
+print("Eh!??!?! Easy mode!?")
+time.sleep(1)
+print("How lame!")
+time.sleep(1)
+print("Only kids play in easy mode!")
+time.sleep(1)
+print("hAHAHAHAHAHA!!!")
