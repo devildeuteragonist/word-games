065704ffc6a8494e06fc62e90fe1820040ae901b
Finity_Cardinal <142924829+devildeuteragonist@users.noreply.github.com>
2024-12-30 22:19:55 +0000

    added list of 1000 most common words

--- the-guessing-game/wordgame-code-easy-mode.py
+++ the-guessing-game/wordgame-code-easy-mode.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3 
 import time
+import random
+
+# importing those 1000 most common words
+with open('1000-common-words.txt', 'r') as f: 
+    words = f.read().splitlines() 
+
+our_words = [word for word in words if len(word) > 3]
 
 time.sleep(1)
 print("Eh!??!?! Easy mode!?")
