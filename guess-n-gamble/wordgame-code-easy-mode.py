dfffbe2af4b39f2c5a8bf490df945be98e1b8f30
Finity_Cardinal <142924829+devildeuteragonist@users.noreply.github.com>
2025-01-04 02:36:50 +0000

    1000 most common words are now as a file imported from the internet instead of a file from a repo

--- guess-n-gamble/wordgame-code-easy-mode.py
+++ guess-n-gamble/wordgame-code-easy-mode.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3 
 import time
 import random
+import urllib 
 
 # importing those 1000 most common words
-with open('1000-common-words.txt', 'r') as f: 
-    words = f.read().splitlines() 
+url = "https://raw.githubusercontent.com/devildeuteragonist/word-games/refs/heads/main/guess-n-gamble/1000-common-words.txt"
+with urllib.request.urlopen(url) as f: 
+    words = f.read().decode("utf-8").splitlines() 
 
 our_words = [word for word in words if len(word) > 3]
 
