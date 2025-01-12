0aa38e131bad3e606263a390349461ee0f7b987e
Finity_Cardinal <142924829+devildeuteragonist@users.noreply.github.com>
2025-01-11 20:19:06 -0500

    temporary file created for debugging

---
+++ guess-n-gamble/guessngambledebugger.py
@@ -0,0 +1,26 @@
+# importing necessary packages
+from flask import Flask, render_template, request, session, redirect, url_for
+import urllib.request
+import time
+import random
+
+# flask gobbledygook
+app = Flask(__name__)
+app.secret_key = "2ykrj6ujukgwe8kvbvxssmffxty9j9"
+app.run(host='0.0.0.0', debug=True)
+
+@app.route("/", methods=["GET","POST"])
+def index():
+    instruction_answer = request.form.get("instruction_answer", "")
+    while instruction_answer not in ["y", "n"]:
+        instruction_message = "Please enter y or n, lowercase with no spaces or punctuation:\n"
+        instruction_answer = request.form["instruction_answer"]
+        return render_template("game_round.html", 
+        message=instruction_message)  
+    if instruction_answer == "y":
+        return render_template("instructions.html")
+    elif instruction_answer == "n":
+        return render_template("index.html", message="Okay. Good luck!")
+        
+if __name__ == "__main__":
+    app.run(debug=True)
