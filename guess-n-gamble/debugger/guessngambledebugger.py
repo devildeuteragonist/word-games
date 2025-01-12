85f9bfaccf2dda4a858039131c194d593674bf7c
Finity_Cardinal <142924829+devildeuteragonist@users.noreply.github.com>
2025-01-12 01:49:07 +0000

    made it easier for debugging purposes

--- guess-n-gamble/debugger/guessngambledebugger.py
+++ guess-n-gamble/debugger/guessngambledebugger.py
@@ -11,19 +11,20 @@ app.run(host='0.0.0.0', debug=True)
 @app.route("/", methods=["GET","POST"])
 def index():
     instruction_answer = request.form.get("instruction_answer", "")
-    while instruction_answer not in ["y", "n"]:
-        instruction_message = "Please enter y or n, lowercase with no spaces or punctuation:\n"
-        instruction_answer = request.form["instruction_answer"]
-        return render_template("game_round.html", 
-        message=instruction_message)  
-    if instruction_answer == "y":
-        return render_template("game_round.html")
-    elif instruction_answer == "n":
-        return render_template("index.html", message="Okay. Good luck!")
+    if request.method=="POST": # this line is added. will have to add everywhere probably. 
+        while instruction_answer not in ["y", "n"]:
+            instruction_message = "Please enter y or n, lowercase with no spaces or punctuation:\n"
+            # removed the variable instruction_answer defined here. (chatgpt suggestion)
+            return render_template("silly.html", 
+            message=instruction_message)  
+        if instruction_answer == "y":
+            return render_template("silly.html")
+        elif instruction_answer == "n":
+            return render_template("index.html", message="Okay. Good luck!")
         
 if __name__ == "__main__":
     app.run(debug=True)
 
-@app.route("/game_round", methods=["GET", "POST"])
+@app.route("/silly", methods="GET")
 def game_round():
-    return render_template("game_round.html")
+    return render_template("silly.html")
