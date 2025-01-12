9f7c466b43368c1280f383ba302d6c6494d207c3
Finity_Cardinal <142924829+devildeuteragonist@users.noreply.github.com>
2025-01-11 22:45:46 -0500

    trying something.

--- guess-n-gamble/guess-n-gamble.py
+++ guess-n-gamble/guess-n-gamble.py
@@ -16,7 +16,7 @@ import random
 # flask gobbledygook
 app = Flask(__name__)
 # this is just for myself: there was a secret key here. i'll pass it as an environment variable or smthn idk. 
-app.run(host='0.0.0.0', debug=True)
+# app.run(host='0.0.0.0', debug=True)
 
 @app.route("/", methods=["GET","POST"])
 def index():
